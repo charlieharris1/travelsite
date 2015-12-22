be85cdf55befe78fe7050a27e9785ac51ba5e68a
Charlie Harris <ceharris92@gmail.com>
2015-12-22 11:10:50 +0000

    cross-page tests now working correctly

--- meadowlark.js
+++ meadowlark.js
@@ -1,62 +1,66 @@
-//  this is the projects entry point (aka app file)
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 
 var app = express();
-// set up handlebars view engine. This creates a view engine and configures Express to use it by default. 
-var handlebars = require('express-handlebars').create({ defaultLayout:'main' }); // default layout main means that unless you specify, this layout will be used for any view.
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-// function is invoked when the route is mapped
-// allows me to override the port by setting the environment variable before the server is started.
 app.set('port', process.env.PORT || 3000);
-// add in the static middleware
-// the static middleware has the same effect as creating a route for each static file you want to deliver that renders a file and returns it to the client. 
+
 app.use(express.static(__dirname + '/public'));
 
+// set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
-	// this line of code means that if test=1 appears in the querystring for any page(and were not running on the production server), the property res.local.showTests will be true.
-	// the res.locals object is part of the context that will be passed to views.
-	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
 	next();
 });
 
-// route
-app.get('/', function (req, res){ // req = request object and res = response object
-	res.render('home')
+app.get('/', function(req, res) {
+	res.render('home');
 });
-
-// about
-app.get('/about', function (req, res){ // get is one of a number of HTTP verbs. GET and POST are the most common. 
+app.get('/about', function(req,res){
 	res.render('about', { 
 		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js'
-	});
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
-
-app.get('/tours/hood-river', function (req, res){
+app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
 });
-
-app.get('tours/request/group-rate', function (req, res){
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate');
 });
 
-// custom 404 page
-app.use(function(req, res){ // app.use is a sort of catch all handler for anything that didn't match a route.
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
 });
 
-//custom 500 page
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-		res.status(500);
-		res.render('500');
-})
+	res.status(500);
+	res.render('500');
+});
 
 app.listen(app.get('port'), function(){
-	console.log('Express started on http://localhost:' + app.get ('port') + ': press Ctrl-C to terminate');
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -3,35 +3,40 @@ var Browser = require('zombie'),
 
 var browser;
 
-suite('Cross-page tests', function(){
-	// setup takes a function that will get executed by the test framework before each test is run. This is where we create a new browser instance for each test.
+suite('Cross-Page Tests', function(){
+
 	setup(function(){
 		browser = new Browser();
 	});
-	test('requesting a group rate quote from the Hood River tour page' + 'should populate the referrer field', function(done){
-		var referrer = 'http://localhost:3000/tours/hood-river';	
-		browser.visit(referrer, function(){ // browser.visit will actually load the page. When the page is loaded, the callback function will be invoked.
-			browser.clickLink('.requestGroupRate', function(){ // this looks for a link with the class name of requestGroupRate and follows it.								
-				assert(browser.field('referrer').value); // the browser.field method returns a DOM Element object which has a value property.		
-					done();
-			});
-		});
-	});
-	test('requesting a group rate quote from the oregon coast tour page' + 'should populate the referrer field', function(done){
-		var referrer = 'http://localhost:3000/tours/oregon-coast';
-		browser.visit(referrer, function(){ 
+
+	test('requesting a group rate quote from the hood river tour page should ' +
+			'populate the hidden referrer field correctly', function(done){
+		var referrer = 'http://localhost:3000/tours/hood-river';
+		browser.visit(referrer, function(){
 			browser.clickLink('.requestGroupRate', function(){
 				assert(browser.field('referrer').value === referrer);
-					done();
+				done();
 			});
 		});
 	});
-	test('requesting the group rate page directly should result' + 'in an empty referrer field', function(done){
-		browser.visit('http://localhost:3000/tours/request-group-rate', function(){
+
+	test('requesting a group rate from the oregon coast tour page should ' +
+			'populate the hidden referrer field correctly', function(done){
+		var referrer = 'http://localhost:3000/tours/oregon-coast';
+		browser.visit(referrer, function(){
 			browser.clickLink('.requestGroupRate', function(){
-				assert(browser.field('referrer').value === '');
-					done();
+				assert(browser.field('referrer').value === referrer);
+				done();
 			});
 		});
 	});
+
+	test('visiting the "request group rate" page dirctly should result ' +
+			'in an empty value for the referrer field', function(done){
+		browser.visit('http://localhost:3000/tours/request-group-rate', function(){
+			assert(browser.field('referrer').value === '');
+			done();
+		});
+	});
+
 });
